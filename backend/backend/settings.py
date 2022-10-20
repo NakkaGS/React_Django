a50081fb1d5acd0ba1a1b19d91714f5996d847e2
NakkaGS <gtnakata@gmail.com>
2022-10-20 07:14:40 +0200

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+# to hide the SECRET_KEY we use the python-decouple (it was created a .env file)
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
