7d058978212ab0c3332051f599dd2443fd19414c
NakkaGS <gtnakata@gmail.com>
2022-11-17 19:42:46 +0100

    Backend: deleteUser created

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -113,7 +113,9 @@ def getUsers(request):
     serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
 
-
-
-
-        
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def deleteUser(request, pk):
+    userForDeletion = User.objects.get(id=pk)
+    userForDeletion.delete()
+    return Response('User was deleted')
