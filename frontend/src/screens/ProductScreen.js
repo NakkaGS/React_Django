bc9274cff0ebdcdd10472275c04e5351eaad5a3c
NakkaGS <gtnakata@gmail.com>
2022-11-24 14:17:44 +0100

    Update ProductScreen.js
    Create review form (it is not working)

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -67,8 +67,17 @@ function ProductScreen({ match }) {
 
     fetchProduct() */
 
-  }, [dispatch, id, match, history, ])
+  }, [dispatch, id, match, history])
 
+  const submitHandler = (e) => {
+      e.preventDefault()
+      dispatch(createProductReview(
+          match.params.id, {
+          rating,
+          comment
+      }
+      ))
+  }
 
   const addtoCardHandler = ( ) => {
     history(`/cart/${id}?qty=${qty}`) //it is using useNavigate, it doesn't need push
@@ -186,7 +195,7 @@ function ProductScreen({ match }) {
                 </Col>
               </Row>
 
-              <Row>
+              <Row className="my-3">
                 <Col md={6}>
                   <h4>Reviews</h4>
 
@@ -201,6 +210,55 @@ function ProductScreen({ match }) {
                       </ListGroup.Item>
                       
                     ))}
+                    
+                    <ListGroup.Item>
+                        <h4>Write a review</h4>
+
+                        {loadingProductReview && <Loader />}
+                        {successProductReview && <Message variant='success'>Review Submitted</Message>}
+                        {errorProductReview && <Message variant='danger'>{errorProductReview}</Message>}
+
+                        {userInfo ? (
+                            <Form onSubmit={submitHandler}>
+                                <Form.Group controlId='rating'>
+                                    <Form.Label>Rating</Form.Label>
+                                    <Form.Control
+                                        as='select'
+                                        value={rating}
+                                        onChange={(e) => setRating(e.target.value)}
+                                    >
+                                        <option value=''>Select...</option>
+                                        <option value='1'>1 - Poor</option>
+                                        <option value='2'>2 - Fair</option>
+                                        <option value='3'>3 - Good</option>
+                                        <option value='4'>4 - Very Good</option>
+                                        <option value='5'>5 - Excellent</option>
+                                    </Form.Control>
+                                </Form.Group>
+
+                                <Form.Group controlId='comment'>
+                                    <Form.Label>Review</Form.Label>
+                                    <Form.Control
+                                        as='textarea'
+                                        row='5'
+                                        value={comment}
+                                        onChange={(e) => setComment(e.target.value)}
+                                    ></Form.Control>
+                                </Form.Group>
+
+                                <Button
+                                    disabled={loadingProductReview}
+                                    type='submit'
+                                    variant='primary'
+                                >
+                                    Submit
+                                </Button>
+
+                            </Form>
+                        ) : (
+                                <Message variant='info'>Please <Link to='/login'>login</Link> to write a review</Message>
+                            )}
+                    </ListGroup.Item>
 
                   </ListGroup>
                 </Col>
