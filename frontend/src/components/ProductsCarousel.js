21e0d18512020ab2bc9e125d4ad8d80c009742e0
NakkaGS <gtnakata@gmail.com>
2022-12-08 07:52:00 +0100

    Frontend: css for the carousel created

--- frontend/src/components/ProductsCarousel.js
+++ frontend/src/components/ProductsCarousel.js
@@ -35,18 +35,18 @@ function ProductsCarousel() {
         <Carousel variant="dark" fade >
         {products?.map(product => ( //it was added ? because it was given fault. It is first asking if the array existed 
 
-          <Carousel.Item key={product._id}>
+          <Carousel.Item key={ product._id }>
             <Link to={`/product/${product._id}`} style={{ textDecoration: 'none' }}>
 
               <Row>
-                <Col md={6} style={{height:"22rem", display: "flex", marginTop: "2.5rem", justifyContent: "center"}}>
-                  <Image src={product.image} alt={product.name} fluid style={{height:"15rem", width:"15rem", borderRadius:"50%"}}/>
+                <Col md={6} className="carousel-col">
+                  <Image src={ product.image } alt={ product.name } fluid className="carousel-image"/>
                 </Col>
 
                 <Col md={4}>
                     <h2>{ product.name }</h2>
                     <p>{ product?.description }</p>
-                    <h3>${product.price}</h3>
+                    <h3>${ product.price }</h3>
                 </Col>
                 
               </Row>
