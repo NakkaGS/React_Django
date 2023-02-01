584f20533bdbc3aaf049d7051df8cc8948ad937a
NakkaGS <gtnakata@gmail.com>
2023-02-01 10:12:00 +0100

    Trying Deploy with Vercel

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
